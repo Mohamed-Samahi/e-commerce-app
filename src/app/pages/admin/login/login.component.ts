064a84d5b77bb31d30d32f4e9a891c0abcef5b03
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2024-05-20 00:04:02 +0300

    login component

--- src/app/pages/admin/login/login.component.ts
+++ src/app/pages/admin/login/login.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  loginData: { userName: string, password: string } = {
+    userName: "",
+    password: "",
+  }
+
+  constructor(private router: Router) {
+
+  }
+
+  onLogin() {
+    if (this.loginData.userName === 'admin' && this.loginData.password === '12345') {
+      this.router.navigateByUrl('/test');
+    } else {
+      alert("Wrong Credentials!");
+    }
+  }
 
 }
