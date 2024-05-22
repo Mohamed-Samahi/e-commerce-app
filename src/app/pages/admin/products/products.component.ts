1b1efdd166b05c93eb229f53fb8276cde82fbae4
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2024-05-22 23:19:45 +0300

    toggle new product side panel

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
+
+  isSidePanelVisible: boolean;
+
+  constructor() {
+    this.isSidePanelVisible = false;
+  }
+
+  toggleSidePanel() {
+    this.isSidePanelVisible = !this.isSidePanelVisible;
+  }
 
 }
